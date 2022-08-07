92661f19e627b8923bfdfd4b71505bd1004a2a06
50ra4 <50ra4.dev.dev@gmail.com>
2022-08-07 15:22:32 +0900

    add: sample test code

---
+++ src/components/SampleComponent.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { SampleComponent } from './SampleComponent';
+
+describe('SampleComponent.tex', () => {
+  test('render App component', () => {
+    render(<SampleComponent />);
+    expect(screen.getByText('Sample Component')).toBeInTheDocument();
+  });
+});
