5b567266ec64251ed0ee45077c0f71b8fcca0664
50ra4 <50ra4.dev.dev@gmail.com>
2022-08-07 17:29:33 +0900

    add: chrome extension manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { crx } from '@crxjs/vite-plugin';
 import { resolve } from 'path';
+
+import manifest from './manifest.config';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, ...config }) => ({
@@ -21,5 +23,5 @@ export default defineConfig(({ command, ...config }) => ({
       },
     },
   },
-  plugins: [react()],
+  plugins: [crx({ manifest })],
 }));
